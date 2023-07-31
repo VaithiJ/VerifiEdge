944562b90fabae02f938b4410ed4aab1339f7494
Jananipriya.M <84500800+janu-riya@users.noreply.github.com>
2023-07-31 15:07:48 +0530

    Update

--- ui/components/edithse.vue
+++ ui/components/edithse.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container  class="personalform">
-      <v-form >
+      <v-form v-model="formValid">
           <h4 class="text-center"> HSE Details</h4>
           <v-alert border="top" color="red lighten-1" dismissible  v-if="fail"> Data insertion failed</v-alert>
           <v-text-field label="Registration Number" v-model="hse_regno" prepend-icon="mdi-notebook" :rules="[rules.required,rules.hse_regno]"></v-text-field>
@@ -16,7 +16,7 @@
         ></v-select>
           <v-file-input @change="fileselect"  label = "Upload Files"  :rules="[rules.required]"></v-file-input>
           <v-container class="text-center">
-          <v-btn text  @click="submit()" color="indigo lighten-2"> Submit </v-btn>
+          <v-btn text  @click="submit()" :disabled="!formValid" class ="button" color="indigo lighten-2"> Submit </v-btn>
       </v-container>
       </v-form>
   </v-container>
@@ -40,6 +40,7 @@ export default{
       hse_school : '',
       fail: null,
       hse_board : '',
+      formValid:null,
       rules : {
         required: (v) => !!v || "Required",
             percents : (v) => (v>=0 && v<=100) || "Value must be between 0 and 100",
