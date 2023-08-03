39525fe2635524129f3f562a9f26b2c17d406c5f
Jananipriya.M <84500800+janu-riya@users.noreply.github.com>
2023-08-03 16:15:40 +0530

    update in personakl details

--- ui/components/persondetailspage.vue
+++ ui/components/persondetailspage.vue
@@ -2,7 +2,10 @@
   <v-container style="width: 100%; ">
     <v-card>
       <v-container v-if="data_">
-        <v-alert type="error" >Personal Data is mandatory.</v-alert>
+        <div class="custom-alert">
+            <v-icon class="alert-icon">mdi-alert-circle</v-icon>
+            <span>SSLC Data is mandatory.</span>
+          </div>
       </v-container>
       <v-card-title>Personal Details</v-card-title>
       <v-card-content>
@@ -110,3 +113,21 @@ methods:{
 }
 }
 </script>
+<style>
+.custom-alert {
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  color: #008cff; 
+}
+
+.custom-alert .alert-icon {
+  color: #008cff; 
+  margin-right: 5px;
+}
+
+.custom-alert .alert-text {
+  font-size: 16px;
+}
+
+</style>
