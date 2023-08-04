4f5527668e4e0f0f42ff5e2b9bb4981d8c84458d
Jananipriya.M <84500800+janu-riya@users.noreply.github.com>
2023-08-04 13:00:14 +0530

    update

--- ui/components/hseviewpage.vue
+++ ui/components/hseviewpage.vue
@@ -2,10 +2,7 @@
   <v-container style="width: 100%; ">
     <v-card>
       <v-container v-if="data_">
-        <div class="custom-alert">
-            <v-icon class="alert-icon">mdi-alert-circle</v-icon>
-            <span>HSE Data is mandatory.</span>
-          </div>
+        <v-alert type="error" >HSE Data is mandatory.</v-alert>
       </v-container>
       <v-card-title>HSE Details</v-card-title>
       <v-card-content>
@@ -174,21 +171,3 @@ export default{
    }
 }
 </script>
-<style>
-.custom-alert {
-  display: flex;
-  align-items: center;
-  background-color: transparent;
-  color: #008cff; 
-}
-
-.custom-alert .alert-icon {
-  color: #008cff; 
-  margin-right: 5px;
-}
-
-.custom-alert .alert-text {
-  font-size: 16px;
-}
-
-</style>
