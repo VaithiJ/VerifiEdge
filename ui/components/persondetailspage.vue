0cb86af2dafb24eada36b226bb7bf2144e65229f
Jananipriya.M <84500800+janu-riya@users.noreply.github.com>
2023-08-04 13:45:51 +0530

    update

--- ui/components/persondetailspage.vue
+++ ui/components/persondetailspage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container style="width: 100%; ">
-    <v-card>
+    <v-card class="curved-box" elevation="12">
       <v-container v-if="data_">
         <div class="custom-alert">
             <v-icon class="alert-icon">mdi-alert-circle</v-icon>
@@ -114,6 +114,10 @@ methods:{
 }
 </script>
 <style>
+.curved-box {
+    border-radius: 40px;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+  }
 .custom-alert {
   display: flex;
   align-items: center;
