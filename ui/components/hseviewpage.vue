1eaecb79051ff98502e1ce3345ad1c719c97f2e2
Jananipriya.M <84500800+janu-riya@users.noreply.github.com>
2023-08-04 14:19:55 +0530

    updation

--- ui/components/hseviewpage.vue
+++ ui/components/hseviewpage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container style="width: 100%; ">
-    <v-card>
+    <v-card class="curved-box" elevation="12">
       <v-container v-if="data_">
         <div class="custom-alert">
             <v-icon class="alert-icon">mdi-alert-circle</v-icon>
@@ -175,6 +175,10 @@ export default{
 }
 </script>
 <style>
+.curved-box {
+    border-radius: 40px;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+  }
 .custom-alert {
   display: flex;
   align-items: center;
