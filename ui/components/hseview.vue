baf493fa044c69131e296e41f2f65d309393d7e4
J Hari Prasad <77690054+HariPrasad1302@users.noreply.github.com>
2023-08-08 13:01:35 +0530

    updation in email sending format

--- ui/components/hseview.vue
+++ ui/components/hseview.vue
@@ -227,7 +227,18 @@ export default{
         let rdata={
           email: email,
           email_subject: "HSE Rejection",
-          email_body: this.email_body,
+          email_body: `
+            Dear Applicant,
+
+            We regret to inform you that your application with HSE Data   has been rejected due to ${this.email_body}. Our team reviewed your application carefully, and unfortunately, we are unable to proceed with your application at this time.
+
+            We appreciate your interest in our services and your effort in applying. Thank you for considering us, and reupload the HSE Data .
+
+            With Regards,
+
+            VerifiEdge
+
+        `
         }
         let nres = await this.$axios.post(rurl, rdata)
         console.log(nres)
