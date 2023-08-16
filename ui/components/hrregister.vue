2e00179d1aab16cf50c172714e5deb1826ac1217
J Hari Prasad <77690054+HariPrasad1302@users.noreply.github.com>
2023-08-16 09:27:51 +0530

    updation

--- ui/components/hrregister.vue
+++ ui/components/hrregister.vue
@@ -56,9 +56,9 @@
                 <v-col cols="12" sm="6">
                     <v-menu v-model="DatePicker" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
                       <template v-slot:activator="{ on }">
-                        <v-text-field v-model="doi" outlined prepend-icon="mdi-calendar" label="Date of Incorporation" readonly v-on="on"></v-text-field>
+                        <v-text-field v-model="user.doi" outlined prepend-icon="mdi-calendar" label="Date of Incorporation" readonly v-on="on"></v-text-field>
                       </template>
-                      <v-date-picker v-model="doi" :max="today" no-title scrollable @input="saveDatePicker"></v-date-picker>
+                      <v-date-picker v-model="user.doi" :max="today" no-title scrollable @input="saveDatePicker"></v-date-picker>
                     </v-menu>
                 </v-col>
                 <v-col cols="12" md="6">
--- ui/components/notaryregister.vue
+++ ui/components/notaryregister.vue
@@ -31,9 +31,9 @@
                 <v-col cols="12" sm="6">
                     <v-menu v-model="DatePicker" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="auto">
                       <template v-slot:activator="{ on }">
-                        <v-text-field v-model="dob" outlined prepend-icon="mdi-calendar" label="Date of Birth" readonly v-on="on"></v-text-field>
+                        <v-text-field v-model="user.dob" outlined prepend-icon="mdi-calendar" label="Date of Birth" readonly v-on="on"></v-text-field>
                       </template>
-                      <v-date-picker v-model="dob" :max="today" no-title scrollable @input="saveDatePicker"></v-date-picker>
+                      <v-date-picker v-model="user.dob" :max="today" no-title scrollable @input="saveDatePicker"></v-date-picker>
                     </v-menu>
                 </v-col>
                 <v-col cols="12" sm="6">
